f4baa28b973c058a29c0fc1fbceaea51c64e4230
qshchenmo <32218502+qshchenmo@users.noreply.github.com>
2023-07-14 15:01:19 +0800

    Update 3door.py

--- 3door.py
+++ 3door.py
@@ -1,30 +1,33 @@
 # -*- coding: utf-8 -*-
 import random
 
+# 模拟次数
 num_simulations = 100000
 
+# 统计计数(选择变换)
 switch_count = 0
-stay_count = 0
 
-# prize is in door 3
+# 房间奖品设置, 以下设置为 3号房间有奖
+prize_room = 3
+no_prize_room = [1, 2]
+
+
 for i in range(num_simulations):
-    # random choose from  door 1,2,3
+    # 随机从 1~3 选择
     selected_door = random.randint(1, 3)
-    if selected_door == 1:
-        # close door 2, we switch to door 3
-        selected_door = 3
-    elif selected_door == 2:
-        # close door 1, we switch to door 3
-        selected_door = 3
-    else:
-        # close door 1 or 2, we switch to door 2 or 1
-        selected_door = random.randint(1, 2)
 
-    if selected_door == 3:
-        # door 3 has prize
+    # 选择到无奖品房间, 由于另一个无奖品房间会被关闭，选择变换可以到有奖房间
+    if selected_door == no_prize_room[0]:
+        selected_door = prize_room
+    # 选择到另一个无奖品房间, 由于另一个无奖品房间会被关闭，选择变换可以到有奖房间
+    elif selected_door == no_prize_room[1]:
+        selected_door = prize_room
+    # 选择到有奖品房间, 一个随机的无奖品房间会被关闭，我们选择另一个
+    elif selected_door == prize_room:
+        selected_door = random.choice(no_prize_room)
+
+    # 如果最终选择到有奖品房间, 中奖计数+1
+    if selected_door == prize_room:
         switch_count += 1
 
-stay_count = num_simulations - switch_count
-
 print("switch win count:", switch_count)
-print("stay win count", stay_count)
